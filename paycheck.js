ab34e9bd2e2f05cefd80fae242c6287ad6d319ee
Harold <haroldcj95@gmail.com>
2019-03-23 09:33:25 -0400

    initial commit

---
+++ paycheck.js
@@ -0,0 +1,5 @@
+class paycheck{
+    constructor(amount){
+        this.amount=amount;
+    }
+}
---
+++ test/paycheck_test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var expect = require('chai').expect;
+var should = require('chai').should();
+
+import payCheck from '../paycheck.js/index.js.js';
+
+describe('paycheck', function() {
+  
+});
