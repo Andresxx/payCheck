bca597f0523b62fdab669d906107000274a3970a
Carlos Jorge Cary Huanca <gary7412@hotmail.com>
2019-06-03 16:55:04 -0400

    refactor notificacion por mail

--- test/notificaciones.test.js
+++ test/notificaciones.test.js
@@ -6,7 +6,7 @@ import {NotificacionPorWPP} from "../src/ReglasDeNegocioEmpresariales/Entidades/
 import {NotificacionPorFacebook} from "../src/ReglasDeNegocioEmpresariales/Entidades/Notificaciones/NotificacionPorFacebook";
 import {NotificacionPorMail} from "../src/ReglasDeNegocioEmpresariales/Entidades/Notificaciones/NotificacionPorMail";
 
-describe('Pruebas de empleado', function() {
+describe('Pruebas de notificacion', function() {
 
     beforeEach(function() {
      });
@@ -23,6 +23,6 @@ describe('Pruebas de empleado', function() {
 
     it('Las notificaciones de e-mail deberian notificar al e-mail', function(){
         let notificacionPorMail = new NotificacionPorMail();
-        expect(notificacionPorMail.notificar()).equal("Se notificara por e-mail al trabajador.");
+        expect(notificacionPorMail.notificar()).equal();
     });
 });
