a32673d8fe38ab4fd7e61229ded64c2f0c2f063e
Carlos Jorge Cary Huanca <gary7412@hotmail.com>
2019-06-03 16:41:53 -0400

    Notificacion por Facebook

--- test/notificaciones.test.js
+++ test/notificaciones.test.js
@@ -3,14 +3,20 @@ var expect = require('chai').expect;
 var should = require('chai').should();
 
 import {NotificacionPorWPP} from "../src/ReglasDeNegocioEmpresariales/Entidades/Notificaciones/NotificacionPorWPP";
+import {NotificacionPorFacebook} from "../src/ReglasDeNegocioEmpresariales/Entidades/Notificaciones/NotificacionPorFacebook";
 
 describe('Pruebas de empleado', function() {
 
     beforeEach(function() {
      });
 
-     it('Las notificaciones de WPP deberian notificar al WPP', function(){
+    it('Las notificaciones de WPP deberian notificar al WPP', function(){
         let notificacionPorWPP = new NotificacionPorWPP();
         expect(notificacionPorWPP.notificar()).equal("Se notificara por WPP al trabajador.");
-    }); 
+    });
+    
+    it('Las notificaciones de Facebook deberian notificar al Facebook', function(){
+        let notificacionPorFacebook = new NotificacionPorFacebook();
+        expect(notificacionPorFacebook.notificar()).equal("Se notificara por Facebook al trabajador.");
+    });
 });
